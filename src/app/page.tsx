2eed3b28c11b589763c99fd4b53ac971cae0dea6
Asma-Abdul-Qadir <asmagigani786@gmail.com>
2024-10-03 01:16:25 +0500

    next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,111 @@
+import Link from "next/link";
 import Image from "next/image";
+import image from "../../public/profile.jpg"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   <>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  {/* -------------------------- Header div -------------------------------- */}
+
+    <div className="bg-blue-400 w-100 h-28 text-right text-black text-2xl font-bold flex justify-evenly p-10">
+     
+      <div className="text-4xl font-bold h-100 text-pink-600">
+
+        
+
+      </div>
+      
+      
+    <Link href={"/About"}> About </Link>
+
+    <Link href={'/Portfolio'}> Portfolio </Link>
+
+    <Link href={'/Contact-us'}> Contact US</Link>
+
+
     </div>
+
+
+
+  {/* -------------------------- Introduction-------------------------------- */}
+
+    <div className='flex'>
+
+    <div className='w-45 h-80 p-10'>
+
+      <h1 className='text-3xl font-bold m-4 text-white-500'>ASMA ABDUL QADIR</h1>
+
+      <h2 className=" text-3xl m-4 font-bold text-pink-500 ml-14"> Web Developer </h2>
+
+    <p className='text-2xl m-2 '>As a curious and skilled web developer and UI/UX designer, I am building innovative digital solutions.With a blend of technical skills and creative flairs,I bring ideas to life. With expertise in both front-end and back-end technologies.</p>
+    </div>
+   
+   {/* -------------------------- Image div -------------------------------- */}
+   
+    <div className=' w-90 h-500 mr-20 mt-10'>
+
+    <Image src={'/profile.jpg'} 
+    width={800}
+    height={500} 
+    alt="picture"   
+    />
+    </div>
+
+</div>
+
+
+      {/* -------------------------- Footer div -------------------------------- */}
+
+ <div className="bg-rgb-500 w-full h-20 mt-auto flex justify-center items-center fixed bottom-0 ">
+
+    <div className="flex justify-evenly p-40 text-black">
+
+ <Link href={'https://www.linkedin.com/in/'}>   
+
+<Image src={'/linkedin.png'}
+      alt="linkedin"
+      width={30}
+      height={30}
+/>
+<p>LinkedIn</p>
+
+</Link>
+</div>
+
+<div className="flex justify-evenly p-40 text-black">
+
+<Link href={'https://github.com/'}>  
+
+<Image src={'/github.jpeg'}
+      alt="github"
+      width={30}
+      height={30}
+/>
+<p>Github</p>
+
+</Link>
+
+</div>
+
+<div className="flex justify-evenly p-40 text-black">
+
+<Link href={'https://www.facebook.com/'}>  
+
+<Image src={'/facebook.png'}
+    alt="facebook"
+    width={30}
+    height={30}
+/>
+<p>Facebook</p>
+
+</Link>
+
+</div>
+
+
+ </div>
+
+   </>
   );
 }
